0ba410364a0d59801e7410e616274fc6a0217cb3
Zhao Peiduo <zhaopeiduo@gmail.com>
2024-03-17 17:02:21 +0800

    Update dataset for OHE label vector and remove resize transform operation

--- datasets/cifar_custom.py
+++ datasets/cifar_custom.py
@@ -12,16 +12,16 @@ class __CIFAR_Customized(Dataset):
     '''
     def __init__(self, root_dir, 
                  cifar_class=None, 
-                 target_size=None,
+                 train=True,
                  transforms=None, 
                  conditional=False, 
                  download=True):
         if cifar_class is None:
             raise NotImplementedError("Please call the subclass or specify cifar_class manually")
-        self.cifar = cifar_class(root_dir, download=download)
+        self.cifar = cifar_class(root_dir,  train=train, download=download)
         self.transforms = transforms
         self.conditional = conditional
-        self.target_size = target_size
+        self.num_classes = max(self.cifar.targets) - min(self.cifar.targets) + 1
 
         # set up transforms
         if self.transforms is not None:
@@ -47,20 +47,15 @@ class __CIFAR_Customized(Dataset):
         image, label = self.cifar[idx]
         image = pil_to_tensor(image)  / 255
 
-        # Standardize resolution to target size
-        if self.target_size:
-            resize_transform = transforms.Resize((self.target_size, self.target_size))
-            image = resize_transform(image)
-
         if self.conditional:
-            # Not sure how to use labels as condition - specified by input_T?
-            condition = label
+            condition = torch.zeros(self.num_classes)
+            condition[label] = 1
             if self.transforms is not None:
-                # TODO: Check how condition works
                 out = self.input_T(image, condition)
                 image = out[0][0]
                 condition = out[1][0]
-                return image, condition # image shape: (Batch, Channel, Height, Width)
+            
+            return image, condition # image shape: (Batch, Channel, Height, Width)
         
         elif self.transforms is not None:
             image = self.input_T(image)[0]
@@ -70,9 +65,9 @@ class __CIFAR_Customized(Dataset):
             return image
     
 class CIFAR10_Customized(__CIFAR_Customized):
-    def __init__(self, root_dir, transforms=None, conditional=False, download=True):
-        super().__init__(root_dir, cifar_class=CIFAR10, transforms=transforms, conditional=conditional, download=download)
+    def __init__(self, root_dir, train=True, transforms=None, conditional=False, download=True):
+        super().__init__(root_dir, cifar_class=CIFAR10, train=train, transforms=transforms, conditional=conditional, download=download)
 
 class CIFAR100_Customized(__CIFAR_Customized):
-    def __init__(self, root_dir, transforms=None, conditional=False, download=True):
-        super().__init__(root_dir, cifar_class=CIFAR100, transforms=transforms, conditional=conditional, download=download)
+    def __init__(self, root_dir, train=True, transforms=None, conditional=False, download=True):
+        super().__init__(root_dir, cifar_class=CIFAR100, train=train, transforms=transforms, conditional=conditional, download=download)
