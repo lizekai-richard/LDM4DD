c9b95ed0a43b0bcf97faf0acea490909d3124668
lizekai-richard <lzknus@gmail.com>
2024-03-19 17:12:10 +0800

    fix bugs

--- diffusion/LatentDiffusion.py
+++ diffusion/LatentDiffusion.py
@@ -63,6 +63,8 @@ class LatentDiffusion(pl.LightningModule):
         self.ae = AutoEncoder()
         with torch.no_grad():
             self.latent_dim = self.ae.encode(torch.ones(1, 3, 256, 256)).shape[1]
+        for p in self.ae.parameters():
+            p.requires_grad = False
         self.model = DenoisingDiffusionProcess(generated_channels=self.latent_dim,
                                                num_timesteps=num_timesteps)
 
@@ -142,6 +144,10 @@ class LatentDiffusionConditional(LatentDiffusion):
         self.ae = AutoEncoder()
         with torch.no_grad():
             self.latent_dim = self.ae.encode(torch.ones(1, 3, 256, 256)).shape[1]
+
+        for p in self.ae.parameters():
+            p.requires_grad = False
+            
         self.model = DenoisingDiffusionConditionalProcess(generated_channels=self.latent_dim,
                                                           condition_channels=self.latent_dim,
                                                           num_timesteps=num_timesteps)
