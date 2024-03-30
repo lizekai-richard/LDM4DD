d32a76c53eed60cf7d06991bce8a0b065b1d0700
Zhao Peiduo <zhaopeiduo@gmail.com>
2024-03-30 13:04:48 +0800

    bug fix; switch to SGD + 1e-2

--- classifier_training/train_utils.py
+++ classifier_training/train_utils.py
@@ -21,21 +21,24 @@ def get_data_loaders(train_data: Dataset, test_data: Dataset, batch_size:int):
     test_data_loader = DataLoader(test_data, batch_size=batch_size, shuffle=False)
     return train_data_loader, test_data_loader
 
-def test_model(model: nn.Module, data_loader: DataLoader):
+def test_model(model: nn.Module, data_loader: DataLoader, criterion):
     labels = []
     predictions = []
     model.eval()
+    test_loss = 0
     with torch.no_grad():
         for data in data_loader:
             image = data[0].to(device)
             label = data[1].to(device)
             pred = model(image)
             pred_class = torch.argmax(pred, dim=1)
+            loss = criterion(pred, label)
+            test_loss += loss.sum()
 
             for l, p in zip(label, pred_class):
                 labels.append(l.item())
                 predictions.append(p.item())
-        return accuracy_score(labels, predictions)
+        return accuracy_score(labels, predictions), test_loss / len(predictions)
 
 def train_model(model: nn.Module, train_data_loader: DataLoader, test_data_loader: DataLoader, optimizer, criterion, model_path, num_epochs=50, model_weights=None):
     """
@@ -49,9 +52,11 @@ def train_model(model: nn.Module, train_data_loader: DataLoader, test_data_loade
         model.load_state_dict(model_weights)
     model.to(device)
 
-    running_loss = 0
+    train_loss = 0
     start_time = datetime.datetime.now()
     best_acc = 0
+    train_record = []
+    test_record = []
 
     for epoch in range(num_epochs):
         model.train()
@@ -62,7 +67,7 @@ def train_model(model: nn.Module, train_data_loader: DataLoader, test_data_loade
             label = data[1].to(device)
             pred = model(image)
             loss = criterion(pred, label)
-            running_loss += loss
+            train_loss += loss
 
             loss.backward()
 
@@ -71,25 +76,26 @@ def train_model(model: nn.Module, train_data_loader: DataLoader, test_data_loade
 
             optimizer.step()
 
-        print(f"epoch: {epoch+1} done, loss: {running_loss / len(train_data_loader.dataset)}")
-        acc = test_model(model, test_data_loader)
+        print(f"epoch: {epoch+1} done, loss: {train_loss / len(train_data_loader.dataset)}")
+        acc, test_loss = test_model(model, test_data_loader, criterion)
         print(f"Acc: {acc}")
         if acc > best_acc:
             best_acc = acc
             torch.save(model.state_dict(), f"{model_path}/model.bin")
-        running_loss = 0
+        train_record.append(train_loss.item())
+        test_record.append(test_loss.item())
+        train_loss = 0
 
     end_time = datetime.datetime.now()
     print(f"Time taken: {(end_time - start_time).total_seconds()} seconds")
+    return train_record, test_record
 
 def train(model, model_path, train_data_loader, test_data_loader):
     criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)  #1e-4 to converge + Adam
-    train_model(model, train_data_loader, test_data_loader, optimizer, criterion, model_path)
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-2)  #1e-4 to converge + Adam
+    return train_model(model, train_data_loader, test_data_loader, optimizer, criterion, model_path)
     
-def test(model, model_path, test_data_loader):
-    model_load_path = f"{model_path}/model.bin"
-    model = torchvision.models.alexnet(num_classes = 100)
-    model.load_state_dict(torch.load(model_load_path, map_location=torch.device('cpu')))
+def test(model, model_path, test_data_loader, criterion):
+    model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
     model.to(device)
-    return test_model(model, test_data_loader)
+    return test_model(model, test_data_loader, criterion)
