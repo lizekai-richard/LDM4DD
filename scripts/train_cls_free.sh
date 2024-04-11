de92e81b2140a40e53d85af1141cf4408dcd186e
lizekai-richard <lzknus@gmail.com>
2024-04-11 13:35:38 +0800

    update

--- scripts/train_cls_free.sh
+++ scripts/train_cls_free.sh
@@ -2,9 +2,8 @@ nohup accelerate launch train_cls_free.py \
 --train_data_path "/mnt/data/zekai/CIFAR10/" \
 --val_data_path "/mnt/data/zekai/CIFAR10/" \
 --save_dir "saved_model/" \
---num_steps 50000 \
+--num_steps 80000 \
 --num_timesteps 1000 \
---lr 1e-4 \
---save_freq 5000 \
---devices "0" \
-> logs/train_cls_free_S50000_T1000.log 2>&1 &
+--lr 2e-4 \
+--save_freq 10000 \
+> logs/train_cls_free_S80000_T1000_LR2E-4.log 2>&1 &
